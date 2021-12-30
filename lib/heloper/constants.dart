95a0992ef1477efb78cc404bc906b182f1834ce5
mohamed.zaytoun <mohamed.hassan.1993y@gmail.com>
2021-12-30 14:07:00 +0200

    prepare icons images , colors and folders

---
+++ lib/heloper/constants.dart
@@ -0,0 +1,98 @@
+import 'package:bus_tracking_task/themes/colors.dart';
+import 'package:flutter/material.dart';
+import 'dart:math' as math;
+
+/// icons
+
+ImageIcon kBuoyIcon = kImageIcon(
+  iconPath: 'assets/icons/buoy.png',
+);
+ImageIcon kSwapIcon = kImageIcon(
+  iconPath: 'assets/icons/swap.png',
+);
+
+ImageIcon kClockIconIcon = kImageIcon(
+  iconPath: 'assets/icons/clock_icon.png',
+);
+ImageIcon kArrowCirculeIcon = kImageIcon(
+  iconPath: 'assets/icons/right-arrow.png',
+);
+ImageIcon kSettingIcon = kImageIcon(
+  iconPath: 'assets/icons/setting.png',
+);
+ImageIcon kSignedIcon = kImageIcon(
+  iconPath: 'assets/icons/signed.png',
+);
+ImageIcon kDirectIngIcon = kImageIcon(
+  iconPath: 'assets/icons/direct-instagram.png',
+);
+
+Icon kBackArrowIcon({double iconSize = 32.0}) {
+  return Icon(
+    Icons.arrow_back,
+    size: iconSize,
+  );
+}
+
+Icon kMenuBarIcon({double iconSize = 32.0}) {
+  return Icon(
+    Icons.menu,
+    size: iconSize,
+  );
+}
+
+Icon kProfileIcon({double iconSize = 32.0}) {
+  return Icon(
+    Icons.person_outline_outlined,
+    size: iconSize,
+  );
+}
+
+Icon kCurrentLocatIcon({double iconSize = 32.0}) {
+  return Icon(
+    Icons.location_searching,
+    size: iconSize,
+  );
+}
+
+Icon kShareIcon({double iconSize = 32.0}) {
+  return Icon(
+    Icons.share_outlined,
+    size: iconSize,
+  );
+}
+
+/// tab icons
+Icon kMapTabIcon({double iconSize = 32.0}) {
+  return Icon(
+    Icons.map_outlined,
+    size: iconSize,
+  );
+}
+
+ImageIcon kWarningTabIcon = kImageIcon(
+  iconPath: 'assets/icons/warning.png',
+);
+
+Icon kCalenderTabIcon({double iconSize = 32.0}) {
+  return Icon(
+    Icons.calendar_today,
+    size: iconSize,
+  );
+}
+
+Icon kGiftTabIcon({double iconSize = 32.0}) {
+  return Icon(
+    Icons.card_giftcard,
+    size: iconSize,
+  );
+}
+
+/// *** tab icons end
+ImageIcon kImageIcon({required String iconPath, double? iconSize = 32.0}) {
+  return ImageIcon(
+    AssetImage(iconPath),
+    size: iconSize,
+    color: kTextBlack,
+  );
+}
---
+++ lib/themes/colors.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+const kEmptyColor = 0x0D000000;
+
+/// basic colors
+const kTeal50 = Color(0xFFE0F2F1);
+const kTeal100 = Color(0xFF3FC1BE);
+const kTeal400 = Color(0xFF26A69A);
+const kGrey900 = Color(0xFF263238);
+const kGrey600 = Color(0xFF546E7A);
+const kGrey200 = Color(0xFFEEEEEE);
+const kGrey400 = Color(0xFF90a4ae);
+const kTextBlack = Color(0xFF4D4D4D);
+const kTextGrayLight = Color(0xFFC4C4C6);
+const kTextGrayTag = Color(0xFFDEDEDE);
+const kTextDisActive = Color(0xFFC4C4C6);
+const kIconColorBlack = Color(0xFF211522);
+
+const kErrorRed = Color(0xFFe74c3c);
+const kColorRed = Color(0xFFF3090B);
+const kSurfaceWhite = Color(0xFFFFFBFA);
+//Color(0xffEAF1E9); // day, month background
+const kLightHomeBG = Color(0xffF2F2F2);
+const kLightBG = Colors.white;
+const kDarkBG = Color(0xff1B1D26);
+const kDarkBgLight = Color(0xff282D39);
+
+///color for tags
+const kColorTagButton = Color(0xffA761C4);
+
+/// color for theme
+const kLightPrimary = Color(0xffA761C4);
+//const kDarkMain = Color(0xff317D0C);
+const kLightAccent = Color(0xFF546E7A);
+const kDarkAccent = Color(0xffF4F5F5);
+
+/// color  boarder
+const kBorderEditField = kLightBG;
+const kBorderBar = Color(0xffDFD8D0);
+const kBorderEditText = Color(0xffF2F2F2);
+const kBottomBarIconColor = Color(0xff7d536d40);
+const kverticalBoxColor = Color(0xffF9F9F9);
